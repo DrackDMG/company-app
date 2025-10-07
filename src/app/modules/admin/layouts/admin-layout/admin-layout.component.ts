b23339ba9cc86718e95dbf73510f03738cf2c3b9
DrackDMG <drackmg@gmail.com>
2025-10-07 17:29:34 -0600

    usar libreria creada

--- src/app/modules/admin/layouts/admin-layout/admin-layout.component.ts
+++ src/app/modules/admin/layouts/admin-layout/admin-layout.component.ts
@@ -1,15 +1,17 @@
 import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SideMenuComponent } from '../../../shared/components/side-menu/side-menu.component';
-
+import { DmgSideMenu, TitleColor} from 'dmg-side-menu';
 @Component({
   selector: 'app-admin-layout',
-  imports: [RouterOutlet, SideMenuComponent],
+  imports: [RouterOutlet, SideMenuComponent, DmgSideMenu],
   templateUrl: './admin-layout.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export default class AdminLayoutComponent {
    isAuthenticated = signal(false);
+
+   TitleColor = TitleColor;
 
   onLogin() {
     this.isAuthenticated.set(true);
